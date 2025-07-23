6cfc424621e159a99c6d3ca576541ec71868c295
hch507 <chunghuihan9@gmail.com>
2025-07-23 22:57:30 +0900

    chore : designsystem Color 정의

---
+++ core/designsystem/src/main/java/com/example/designsystem/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.example.keypick_compose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val Purple70 = Color(0xFFDA8AF0) // purple_line
+val Purple50 = Color(0xFF9D51EA) // blog
+val Purple40 = Color(0xFF6650a4)
+val Purple30 = Color(0xFF9D51EA)
+val Purple10 = Color(0xFF352E39) // purple_circle
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink80 = Color(0xFFEFB8C8)
+val Pink40 = Color(0xFF7D5260)
+val Teal70 = Color(0xFF018786)
+val Teal60 = Color(0xFF25D8C9) // mobile
+val Teal20 = Color(0xff6074f9)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val Red30 = Color(0xFFF96060)  // appbar
+val Gray90 = Color(0xFFFBFBFB) // background
+val Gray20 = Color(0xFF313131) // line
+val Blue60 = Color(0xFF526DF8) // month
