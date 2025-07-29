b244b43cfaefa665235313e6320af08b11d920f5
hch507 <chunghuihan9@gmail.com>
2025-07-29 20:28:27 +0900

    chore : KotlinAndroid 정의

---
+++ build-logic/convention/src/main/java/com/example/convention/KotlinAndroid.kt
@@ -0,0 +1,22 @@
+package com.example.convention
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+
+internal fun Project.configureKotlinAndroid(
+    commonExtension: CommonExtension<*, *, *, *, *, *>
+) {
+    commonExtension.apply {
+        compileSdk = 35
+
+        defaultConfig {
+            minSdk = 21
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+    }
+}
