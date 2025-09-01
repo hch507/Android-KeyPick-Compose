d9860d4d67c3f29d24afad835e08f67b416d6a59
hch507 <chunghuihan9@gmail.com>
2025-09-01 20:47:59 +0900

    chore : feature/nonlogin 모듈 추가

---
+++ feature/nonlogin/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    alias(libs.plugins.keypick.android.feature)
+}
+
+android {
+    namespace = "com.example.nonlogin"
+
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
