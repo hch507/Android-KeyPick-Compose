9763052e71b1b748ae3c7f64a480c8981cb69029
Han Chunghee <chunghuihan9@gmail.com>
2025-09-17 15:40:08 +0900

    [chore] : feature/ranking 모듈 필수 의존성 추가

--- feature/ranking/build.gradle.kts
+++ feature/ranking/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.keypick.android.feature)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -30,6 +32,8 @@ android {
 
 dependencies {
 
+    implementation(projects.core.data)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -38,4 +42,12 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.tooling.preview)
+
+    ksp(libs.hilt.compiler)          // Hilt
+    implementation(libs.hilt.android)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation (libs.androidx.runtime)
+
+    implementation (libs.androidx.hilt.navigation.compose)
 }
