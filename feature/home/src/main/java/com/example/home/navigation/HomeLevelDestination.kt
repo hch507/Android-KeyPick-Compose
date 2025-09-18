124d1046e592bdd8f6d4cba9bed7b65d574f682b
Han Chunghee <chunghuihan9@gmail.com>
2025-09-18 10:48:35 +0900

    [refactor] :  homeLevelDestination 수정

--- feature/home/src/main/java/com/example/home/navigation/HomeLevelDestination.kt
+++ feature/home/src/main/java/com/example/home/navigation/HomeLevelDestination.kt
@@ -9,18 +9,15 @@ import kotlin.reflect.KClass
 enum class HomeLevelDestination(
     @StringRes val titleText : Int,
     val route : KClass<*>,
-    val baseRoute : KClass<*> = route
 ){
 
     USER_BLOG_INFO(
         titleText = R.string.home_title,
         route = UserBlogInfoRoute::class,
-        baseRoute = UserBlogInfoRoute::class,
     ),
     RANKING(
         titleText = R.string.rank_title,
         route = RankingRoute::class,
-        baseRoute = RankingRoute::class,
     )
 
 }
