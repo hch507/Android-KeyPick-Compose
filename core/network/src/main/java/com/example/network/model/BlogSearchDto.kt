d5448c0356bbd3f282df5e94766a784112ec668a
Han Chunghee <chunghuihan9@gmail.com>
2025-09-22 13:39:11 +0900

    [feat] :  corr/network dto mapper 추가(network -> model)

--- core/network/src/main/java/com/example/network/model/BlogSearchDto.kt
+++ core/network/src/main/java/com/example/network/model/BlogSearchDto.kt
@@ -1,23 +1,34 @@
 package com.example.network.model
 
+import com.keypick.core.model.BlogData
+import com.keypick.core.model.KeywordBlogInfoResource
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class BlogSearchDto(
-    @SerialName("display")val display: Int,
-    @SerialName("items")val items: List<Item>,
-    @SerialName("lastBuildDate")val lastBuildDate: String,
-    @SerialName("start")val start: Int,
-    @SerialName("total")val total: Int
+    @SerialName("display") val display: Int,
+    @SerialName("items") val items: List<Item>,
+    @SerialName("lastBuildDate") val lastBuildDate: String,
+    @SerialName("start") val start: Int,
+    @SerialName("total") val total: Int
 )
 
 @Serializable
 data class Item(
-    @SerialName("bloggerlink")val bloggerlink: String,
-    @SerialName("bloggername")val bloggername: String,
-    @SerialName("description")val description: String,
-    @SerialName("link")val link: String,
-    @SerialName("postdate")val postdate: String,
-    @SerialName("title")val title: String
+    @SerialName("bloggerlink") val bloggerlink: String,
+    @SerialName("bloggername") val bloggername: String,
+    @SerialName("description") val description: String,
+    @SerialName("link") val link: String,
+    @SerialName("postdate") val postdate: String,
+    @SerialName("title") val title: String
+)
+
+fun BlogSearchDto.asExternalModel(): KeywordBlogInfoResource = KeywordBlogInfoResource(
+    totalCnt = total,
+    blogData = items.map { item ->
+        BlogData(
+            date = item.postdate,
+        )
+    }
 )
--- core/network/src/main/java/com/example/network/model/MonthlySearchDto.kt
+++ core/network/src/main/java/com/example/network/model/MonthlySearchDto.kt
@@ -1,5 +1,7 @@
 package com.example.network.model
 
+import com.keypick.core.model.MonthRatioResource
+import com.keypick.core.model.RatioData
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -18,8 +20,22 @@ data class Result(
     @SerialName("keywords") val keywords: List<String>,
     @SerialName("title") val title: String
 )
+
 @Serializable
 data class Data(
     @SerialName("period") val period: String,
     @SerialName("ratio") val ratio: Double
 )
+
+fun MonthlySearchDto.asExternalModel(): List<MonthRatioResource> =
+    results.map {
+        MonthRatioResource(
+            title = it.title,
+            ratioData = it.data.map { data ->
+                RatioData(
+                    period = data.period,
+                    rate = data.ratio
+                )
+            }
+        )
+    }
