1c191304643b6a1cc752ab7aeff8a061156a7c2f
hch507 <chunghuihan9@gmail.com>
2025-09-23 08:52:04 +0900

    Update UserBlogInfoRoute.kt
    [feat] :feature/UserBlogInfo 와이어프레임 v1

--- feature/userbloginfo/src/main/java/com/example/userbloginfo/UserBlogInfoRoute.kt
+++ feature/userbloginfo/src/main/java/com/example/userbloginfo/UserBlogInfoRoute.kt
@@ -1,17 +1,44 @@
 package com.example.userbloginfo
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.example.userbloginfo.navigation.UserBlogInfoRoute
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 internal fun UserBlogInfoRoute(
 
-){
-
+) {
+    UserBlogInfoScreen()
 }
 
 
 @Composable
-fun UserBlogInfoScreen(){
+fun UserBlogInfoScreen(
 
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(30.dp)
+    ) {
+        Column {
+            Text("내 블로그", fontSize = 20.sp, modifier = Modifier.fillMaxWidth())
+            Text("ddoaak님의 블로그 현황을 알려드릴게요.", fontSize = 10.sp)
+            Text(
+                "2025.08.08",
+                fontSize = 10.sp,
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(10.dp)
+            )
+        }
+    }
 }
