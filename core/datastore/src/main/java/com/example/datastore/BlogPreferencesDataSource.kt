2e9247df70bd9e98031a876b208158d4571f5327
Han Chunghee <chunghuihan9@gmail.com>
2025-09-25 13:06:01 +0900

    [feat] : core/datastore getBlogId 기능 추가

--- core/datastore/src/main/java/com/example/datastore/BlogPreferencesDataSource.kt
+++ core/datastore/src/main/java/com/example/datastore/BlogPreferencesDataSource.kt
@@ -1,11 +1,15 @@
 package com.example.datastore
 
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class BlogPreferencesDataSource @Inject constructor(
     private val blogPreferencs: BlogPreferencs
 ) {
 
+    suspend fun getBlogId() : String{
+        return blogPreferencs.getBlogId()
+    }
     suspend fun saveBlogId(blogId: String) {
         blogPreferencs.setBlogId(blogId)
     }
