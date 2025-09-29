1fda3e1a7a8bd6052b180b68fd4f633db2f21de1
hch507 <chunghuihan9@gmail.com>
2025-09-29 23:14:27 +0900

    [chore] : 의존성 추가

--- feature/ranking/build.gradle.kts
+++ feature/ranking/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
 
     implementation(projects.core.data)
     implementation(projects.core.model)
+    implementation(projects.core.domain)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
