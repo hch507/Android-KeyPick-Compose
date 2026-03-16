993195663dc25117f05e46ae0a3be3faf900fba3
hch507 <chunghuihan9@gmail.com>
2026-03-16 10:24:19 +0900

    [chore] : feature/search model 의존성 추가

--- feature/search/build.gradle.kts
+++ feature/search/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 
 dependencies {
 
+    implementation(projects.core.model)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
