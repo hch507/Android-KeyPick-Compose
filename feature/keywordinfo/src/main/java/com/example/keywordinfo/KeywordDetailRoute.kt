a451e594bfd85b073e54bbb53a7361a21a6b2fea
hch507 <chunghuihan9@gmail.com>
2026-03-19 20:25:55 +0900

    [feat] : KeywordDetailRoute, RelatedKeywordsRoute ui 구현

--- feature/keywordinfo/src/main/java/com/example/keywordinfo/KeywordDetailRoute.kt
+++ feature/keywordinfo/src/main/java/com/example/keywordinfo/KeywordDetailRoute.kt
@@ -1,10 +1,35 @@
 package com.example.keywordinfo
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.designsystem.card.AppCard
+import com.example.designsystem.chart.KeyPickChart
 
 
 @Composable
@@ -16,8 +41,185 @@ fun KeywordDetailRoute(viewModel: KeywordInfoViewModel){
 fun KeywordDetailScreen(
 ){
 
-    Column {
-        Text(text =" KeywordDetailScreen")
+    Box(
+
+    ){
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFF4F6FB)),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            item {
+                KeywordCard(keyword = "베이퍼 맥스")
+            }
+            item {
+                StatRow(
+                    leftTitle = "PC 검색",
+                    leftValue = "4241",
+                    leftIcon =  painterResource(com.example.designsystem.R.drawable.ic_pc_item),
+                    rightTitle = "모바일 검색",
+                    rightValue = "4135",
+                    rightIcon = painterResource(com.example.designsystem.R.drawable.ic_mobile_item),
+
+                )
+            }
+            item {
+                StatRow(
+                    leftTitle = "블로그",
+                    leftValue = "14856121",
+                    leftIcon = painterResource(com.example.designsystem.R.drawable.ic_blog_item),
+                    rightTitle = "포스팅",
+                    rightValue = "855",
+                    rightIcon = painterResource(com.example.designsystem.R.drawable.ic_posting_tiem)
+                )
+            }
+            item {
+                AppCard (
+                    modifier = Modifier
+                ) {
+                    Column() {
+                        Text(
+                            "최근 5일 방문자 분석",
+                            color = Color.Gray,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+                        KeyPickChart(
+                            visitors = listOf(10.0f,10.0f,10.0f,10.0f,10.0f,),
+                            labels = listOf("4일 전", "3일 전", "2일 전", "1일 전", "오늘"),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(250.dp)
+                        )
+                    }
+                }
+            }
+        }
     }
 
+}
+
+@Composable
+fun KeywordCard(keyword: String){
+    AppCard(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column {
+
+            // 제목 + 아이콘
+            Row(
+                modifier = Modifier.fillMaxWidth().height(30.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Text(
+                    text = "키워드 명",
+                    color = Color.Gray,
+                    fontSize = 14.sp
+                )
+
+                Icon(
+                    painter = painterResource(com.example.designsystem.R.drawable.ic_book_mark),
+                    contentDescription = null,
+                    tint = Color.Unspecified,
+                    modifier = Modifier
+                        .fillMaxHeight()   // 높이 1/3
+                        .aspectRatio(1f)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = keyword,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
+    }
+}
+
+@Composable
+fun StatRow(
+    leftTitle: String,
+    leftValue: String,
+    leftIcon : Painter,
+    rightTitle: String,
+    rightValue: String,
+    rightIcon : Painter
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+
+        StatCard(
+            title = leftTitle,
+            value = leftValue,
+            icon = leftIcon,
+            modifier = Modifier.weight(1f)
+        )
+
+        StatCard(
+            title = rightTitle,
+            value = rightValue,
+            icon = rightIcon,
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+fun StatCard(
+    title: String,
+    value: String,
+    icon: Painter,
+    modifier: Modifier = Modifier
+) {
+    AppCard(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            // 제목 + 아이콘
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Text(
+                    text = title,
+                    color = Color.Gray,
+                    fontSize = 14.sp
+                )
+
+                Icon(
+                    painter = icon,
+                    contentDescription = null,
+                    tint = Color.Unspecified
+                )
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = value,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+
+            Text(
+                text = "30일 기준",
+                color = Color.Gray,
+                fontSize = 12.sp
+            )
+        }
+    }
 }
--- feature/keywordinfo/src/main/java/com/example/keywordinfo/RelatedKeywordsRoute.kt
+++ feature/keywordinfo/src/main/java/com/example/keywordinfo/RelatedKeywordsRoute.kt
@@ -1,43 +1,54 @@
 package com.example.keywordinfo
 
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.designsystem.theme.KeypickComposeTheme
+import com.example.designsystem.card.AppCard
 import com.keypick.core.model.KeywordInfo
 import com.keypick.core.model.RelKewordResource
+
 
 @Composable
 fun RelatedKeywordsRoute(
     viewModel: KeywordInfoViewModel
 ) {
+    val context = LocalContext.current
     val keywordInfoState by viewModel.keywordInfoState.collectAsStateWithLifecycle()
     RelatedKeywordsScreen(keywordInfoState, onSaveClick = {
         viewModel.saveKeyword(it)
+        Toast.makeText(
+            context,
+            "저장되었습니다",
+            Toast.LENGTH_SHORT
+        ).show()
     })
 }
 
@@ -77,28 +88,30 @@ fun KeywordInfoSuccessScreen(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(vertical = 15.dp)
+            .background(Color(0xFFF4F6FB)),
     ) {
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+
             verticalArrangement = Arrangement.spacedBy(12.dp) // 아이템 간 간격
         ) {
 
-            item { KeywordTitle(relKeywordResourceList[0]) }
+            item { RelKeywordCard(relKeywordResourceList[0].relKeyword, onSaveClick = onSaveClick) }
             item { Spacer(modifier = Modifier.height(25.dp)) }
             stickyHeader {
                 Box(
                     modifier = Modifier
                         .background(
-                            MaterialTheme.colorScheme.background
+                            Color(0xFFF4F6FB)
                         )
                         .padding(vertical = 10.dp)
                 ) {
                     Row(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(15.dp)
+
                     ) {
                         Text(text = "연관 키워드", fontSize = 15.sp, modifier = Modifier.weight(1f))
                         Text(text = "${relKeywordResourceList.size - 1}개", fontSize = 15.sp)
@@ -114,23 +127,59 @@ fun KeywordInfoSuccessScreen(
     }
 }
 
-@Composable
-fun KeywordTitle(
-    relKeywordResource: RelKewordResource,
-) {
-    RelatedItemBox {
-        Column {
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                RelatedItemTitle()
-                Spacer(modifier = Modifier.weight(1f))
-                Text(text = "${relKeywordResource.monthlyPcQcCnt + relKeywordResource.monthlyMobileQcCnt}회")
-            }
-            Spacer(Modifier.height(10.dp))
-            Text(text = relKeywordResource.relKeyword, fontSize = 20.sp)
-        }
 
+@Composable
+fun RelKeywordCard(
+    keyword: String,
+    onSaveClick: (String) -> Unit
+) {
+    AppCard(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column {
+
+            // 제목 + 아이콘
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(30.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Text(
+                    text = "키워드 명",
+                    color = Color.Gray,
+                    fontSize = 14.sp
+                )
+
+                IconButton(
+                    onClick = {
+                        onSaveClick(keyword)
+                    },
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .aspectRatio(1f)
+                ) {
+                    Icon(
+                        painter = painterResource(com.example.designsystem.R.drawable.ic_book_mark),
+                        contentDescription = null,
+                        tint = Color.Unspecified,
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .aspectRatio(1f)
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = keyword,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
     }
 }
 
@@ -139,85 +188,62 @@ fun RelatedItem(
     relKeywordResource: RelKewordResource,
     onSaveClick: (String) -> Unit
 ) {
-    RelatedItemBox() {
-        Column(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            RelatedItemTitle()
-            Spacer(Modifier.height(10.dp))
-            Text(text = relKeywordResource.relKeyword, fontSize = 20.sp)
-            Spacer(Modifier.height(10.dp))
-            Text(
-                text = relKeywordResource.monthlyPcQcCnt + relKeywordResource.monthlyMobileQcCnt,
-                fontSize = 15.sp,
-                modifier = Modifier.align(Alignment.End) // 👈 이 한 줄로 오른쪽 정렬됨
-            )
-            Spacer(Modifier.height(10.dp))
-            Button(
-                onClick = { onSaveClick(relKeywordResource.relKeyword) }
+    AppCard(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column {
+            // 제목 + 아이콘
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(30.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text("Save")
+
+                Text(text = relKeywordResource.relKeyword, fontSize = 20.sp)
+                IconButton(
+                    onClick = {
+                        onSaveClick(relKeywordResource.relKeyword)
+                    },
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .aspectRatio(1f)
+                ) {
+                    Icon(
+                        painter = painterResource(com.example.designsystem.R.drawable.ic_book_mark),
+                        contentDescription = null,
+                        tint = Color.Unspecified,
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .aspectRatio(1f)
+                    )
+                }
+            }
+
+            Spacer(Modifier.height(10.dp))
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(com.example.designsystem.R.drawable.ic_releted_search),
+                    contentDescription = null,
+                    tint = Color.Unspecified
+                )
+                Text(
+                    text = "${relKeywordResource.monthlyPcQcCnt + relKeywordResource.monthlyMobileQcCnt}회",
+                    color = Color.Gray,
+                    fontSize = 15.sp,
+                )
+
             }
 
         }
     }
 }
 
-@Composable
-fun RelatedItemBox(
-    modifier: Modifier = Modifier.padding(horizontal = 15.dp),
-    content: @Composable () -> Unit
-) {
-    Box(
-        modifier = modifier
-            .shadow(4.dp, shape = RoundedCornerShape(20.dp))
-            .background(
-                color = Color.White,
-                shape = RoundedCornerShape(20.dp)
-            )
-    ) {
-        Box(modifier = Modifier.padding(16.dp)) {
-            content()
-        }
-    }
-}
 
-@Composable
-fun RelatedItemTitle(
-    modifier: Modifier = Modifier
-) {
-    Box(
-        modifier = Modifier
-            .shadow(4.dp, shape = RoundedCornerShape(20.dp))
-            .background(
-                color = Color.Blue,
-                shape = RoundedCornerShape(30.dp)
-            )
-            .padding(vertical = 3.dp, horizontal = 10.dp)
-    ) {
-        Text(text = "키워드 명", fontSize = 15.sp, color = Color.White)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun RelatedItemBoxPreview() {
-    KeypickComposeTheme {
-        RelatedItemBox {
-            Text(text = "연관 키워드 아이템")
-        }
-    }
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun RelatedItemTitlePreview() {
-    KeypickComposeTheme {
-        RelatedItemTitle()
-    }
-
-}
 //@Preview(showBackground = true)
 //@Composable
 //fun RelatedItemPreview() {
