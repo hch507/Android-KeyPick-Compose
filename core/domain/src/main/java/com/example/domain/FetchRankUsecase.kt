ab8d41e51d98467fde7fb5248cf4bc2ec57d1a55
hch507 <chunghuihan9@gmail.com>
2026-03-30 12:41:57 +0900

    [refactor] : FetchRankUsecase html 태그 제거

--- core/domain/src/main/java/com/example/domain/FetchRankUsecase.kt
+++ core/domain/src/main/java/com/example/domain/FetchRankUsecase.kt
@@ -1,21 +1,27 @@
 package com.example.domain
 
+import android.os.Build
 import com.example.data.repository.KeywordRepository
 import com.keypick.core.model.Rank
 import com.keypick.core.model.MyRank
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
+import android.text.Html
+import androidx.annotation.RequiresApi
 
 class FetchRankUsecase @Inject constructor(
     private val keywordRepository: KeywordRepository
 ) {
 
+    @RequiresApi(Build.VERSION_CODES.N)
     suspend operator fun invoke(keyword: String, blogId: String): Flow<Rank> {
         return keywordRepository.fetchBlogPostRank(keyword)
             .map { allRank ->
 
-
+                val cleanedList = allRank.map {
+                    it.copy(title = it.title.htmlToPlainText())
+                }
 
                 val foundItem = allRank.firstOrNull {
                     it.link.contains(blogId, ignoreCase = true)
@@ -24,7 +30,7 @@ class FetchRankUsecase @Inject constructor(
                 val myRank = if (foundItem != null) {
                     MyRank(
                         myRank = foundItem.rank.toString(),
-                        title = foundItem.title
+                        title = foundItem.title.htmlToPlainText()
                     )
                 } else {
                     MyRank(
@@ -35,10 +41,15 @@ class FetchRankUsecase @Inject constructor(
 
                 Rank(
                     myRank = myRank,
-                    allRank = allRank
+                    allRank = cleanedList
                 )
             }
     }
 
 
+}
+
+@RequiresApi(Build.VERSION_CODES.N)
+fun String.htmlToPlainText(): String {
+    return Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY).toString()
 }
